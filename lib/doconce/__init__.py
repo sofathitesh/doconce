43a3e3a8d6a6aecd851e0c78be992345fbce37fb
Johannes Ring <johannr@simula.no>
2011-09-30 11:40:02 +0200

    Set version to 0.5.1.

--- lib/doconce/__init__.py
+++ lib/doconce/__init__.py
@@ -36,7 +36,7 @@ Documentation of Doconce is found in
 Both directories contain a make.sh file for creating various formats.
 '''
 
-__version__ = '0.5'
+__version__ = '0.5.1'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Johannes H. Ring'
 author = __author__
