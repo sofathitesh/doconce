f74ab46f0ed94c77ff9c909a48865cb9b9b2967d
Hans Petter Langtangen <hpl@simula.no>
2011-10-03 09:19:56 +0200

    Added tests for encodings in test dir.

--- test/clean.sh
+++ test/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files="tmp_* *~ test.v verify* testdoc.gwiki testdoc.html testdoc.rst* testdoc.txt testdoc.epytext testdoc*.tex testdoc.st testdoc.sphinx.rst *.log sphinx-rootdir"
+files="tmp_* *~ test.v verify* testdoc.gwiki testdoc.html testdoc.rst* testdoc.txt testdoc.epytext testdoc*.tex testdoc.st testdoc.sphinx.rst *.log sphinx-rootdir tmp_encoding.txt tmp1.do.txt tmp2.do.txt"
 ls $files 2> /dev/null
 rm -rf $files
 rm -f test.v
