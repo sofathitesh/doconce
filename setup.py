62deb471cad3a32b7dd2e5ff65c2c682fdd53a6a
Hans Petter Langtangen <hpl@simula.no>
2011-12-29 11:34:39 +0100

    Fixes.

--- setup.py
+++ setup.py
@@ -42,13 +42,13 @@ setup(
     package_dir = {'': 'lib'},
     packages = ['doconce'],
     # list individual modules since .p.py and _update.py etc. are not
-    # to be included in an official distribution (does not work
-    # with package_data)
+    # to be included in an official distribution (this does not work
+    # with package_data - must just specify the package name)
     #py_modules = ['doconce.common', 'doconce.doconce', 'doconce.latex',
     #              'doconce.rst', 'doconce.sphinx',
     #              'doconce.st', 'doconce.plaintext',
     #              'doconce.html', 'doconce.epytext',
-    #              'doconce.DocWriter', 'doconce.gwiki',
+    #              'doconce.DocWriter', 'doconce.gwiki', 'doconce.mwiki',
     #              #'doconce.pandoc',
     #             'doconce.expand_newcommands',
     #             ],
