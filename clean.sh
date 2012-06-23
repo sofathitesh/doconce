c1ac43159f1fd0e0b234d1dead7ae9ac6248f168
Hans Petter Langtangen <hpl@simula.no>
2012-06-23 13:59:15 +0200

    Added demos and global clean script.

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd doc/manual; sh clean.sh; cd ../..
+cd doc/tutorial; sh clean.sh; cd ../..
+cd doc/quickref; sh clean.sh; cd ../..
+cd test; sh clean.sh; cd ..
+
+rm -rf doc/api-source/sphinx-rootdir/_build
+
