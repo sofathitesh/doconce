2a7eec6cbc030b207dfca357c46364a3f8cbc5f2
Hans Petter Langtangen <hpl@simula.no>
2012-12-13 14:30:04 -0800

    updates

--- lib/doconce/misc.py
+++ lib/doconce/misc.py
@@ -25,7 +25,7 @@ def recommended_html_styles_and_pygments_styles():
         'sky': ['default'],
         'night': ['fruity', 'native'],
         'darkgrey': ['native', 'monokai'],
-        'serif': ['native', 'monokai'],
+        'serif': ['perldoc'],
         },
         'csss': {
         'csss_default': ['monokai'],
