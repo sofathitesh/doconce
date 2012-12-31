719cf0974ec40a765b2d4a5d84c00f1daee662e2
Hans Petter Langtangen <hpl@simula.no>
2012-12-31 02:18:11 -0800

    updates

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd doc/manual; sh clean.sh; cd ../..
-cd doc/tutorial; sh clean.sh; cd ../..
-cd doc/quickref; sh clean.sh; cd ../..
-cd test; sh clean.sh; cd ..
-
-rm -rf doc/api-source/sphinx-rootdir/_build
-rm -f lib/doconce/docstrings/docstring.dst.txt
-
