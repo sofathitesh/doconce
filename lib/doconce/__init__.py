2ae1b478f37929dff48a5ce02aab7c29f2b3d231
Hans Petter Langtangen <hpl@simula.no>
2013-01-06 19:13:17 -0800

    updates

--- lib/doconce/__init__.py
+++ lib/doconce/__init__.py
@@ -88,7 +88,7 @@ Both directories contain a make.sh file for creating various formats.
 
 '''
 
-__version__ = '0.8.0'
+__version__ = '0.8.2'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Johannes H. Ring'
 author = __author__
