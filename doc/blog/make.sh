0139a5e01efc64b37bfec080a0d7dadb4af13e6b
Hans Petter Langtangen <hpl@simula.no>
2013-01-16 16:44:34 -0800

    Updated blog demo for wordpress.

--- doc/blog/make.sh
+++ doc/blog/make.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-doconce format html demo
-pygmentize -l html -f html -O full,style=default -o demo.html.html demo.html
 pygmentize -l text -f html -O full,style=default -o demo.do.txt.html demo.do.txt
+
+# wordpress.com
+doconce format html demo -DBLOG=wordpress --wordpress
+cp demo.html demo_wordpress.html
+doconce subst 'defined in <b>.+' '' demo_wordpress.html  # refs don't work
+pygmentize -l html -f html -O full,style=default -o demo_wordpress.html.html demo_wordpress.html
+# fix
+
+# Google Blogger
+doconce format html demo -DBLOG=google
+pygmentize -l html -f html -O full,style=default -o demo.html.html demo.html
 
 doconce format mwiki demo
 
 # need to update doconce.blogspot.no with new text
+# and doconce.wordpress.com
 # and http://doconcedemo.jumpwiki.com/wiki/First_demo
 # and test in http://en.wikibooks.org/wiki/Sandbox
 
