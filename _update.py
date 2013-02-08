dccad395d48e87de2530b4170e29aa759034e86b
Hans Petter Langtangen <hpl@simula.no>
2013-02-08 01:36:24 -0800

    updates

---
+++ _update.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+import sys
+
+def system(cmd):
+    print cmd
+    failure = os.system(cmd)
+    if failure:
+        print 'could not run\n%s\nin%s' % (cmd, os.getcwd())
+        sys.exit(1)
+
+def rmtree(directory):
+    try:
+        shutil.rmtree(directory)
+    except OSError:
+        pass # OK if the directory does not exist
+
+def zip_dir(dirname):
+    os.system('rm -rf %s/.git' % dirname)  # remove .git, all will be removed...
+    system('zip -r %s.zip %s' % (dirname, dirname))
+
+if __name__ == '__main__':
+    import os, shutil, glob
+    software_dir = 'bundled'
+    os.chdir(software_dir)
+    system('sh clean.sh')
+    # pack zip files distributed as data with doconce
+    system('zip -r sphinx_themes.zip sphinx_themes')
+    system('zip -r html_images.zip html_images')
+    rmtree('reveal.js')
+    system('git clone git://github.com/hakimel/reveal.js.git')
+    os.system('cp doconce_modifications/reveal/css/reveal*.css reveal.js/css/')
+    os.system('cp doconce_modifications/reveal/css/theme/*.css reveal.js/css/theme/')
+    zip_dir('reveal.js')
+
+    rmtree('csss')
+    system('git clone git://github.com/LeaVerou/csss.git')
+    os.system('cp doconce_modifications/csss/*.css csss/')
+    zip_dir('csss')
+
+    rmtree('deck.js')
+    system('git clone git://github.com/imakewebthings/deck.js.git')
+    rmtree('mnml')
+    system('git clone git://github.com/duijf/mnml.git')
+    system('cp mnml/mnml.css deck.js/themes/style')
+    rmtree('deckjs-theme-mozilla')
+    system('git clone git://github.com/groovecoder/deckjs-theme-mozilla.git')
+    system('cp deckjs-theme-mozilla/*.*css deck.js/themes/style')
+    rmtree('deck.js-codemirror')
+    system('git clone git://github.com/iros/deck.js-codemirror.git')
+    os.mkdir('deck.js/extensions/codemirror')
+    system('cp -r deck.js-codemirror/* deck.js/extensions/codemirror/')
+    rmtree('deck.ext.js')
+    system('git clone git://github.com/barraq/deck.ext.js.git')
+    system('cp -r deck.ext.js/themes/style/*.*css deck.js/themes/style/')
+    rmtree('deck.pointer.js')
+    system('git clone git://github.com/mikeharris100/deck.pointer.js.git')
+    os.mkdir('deck.js/extensions/pointer')
+    system('cp -r deck.pointer.js/deck.pointer.* deck.js/extensions/pointer/')
+    rmtree('presenterview')
+    system('git clone git://github.com/stvnwrgs/presenterview.git')
+    system('cp -r presenterview/ deck.js/extensions/')
+    rmtree('deck.annotate.js')
+    system('git clone git://github.com/nemec/deck.annotate.js.git')
+    system('cp -r deck.annotate.js deck.js/extensions/')
+
+    system('cp doconce_modifications/deck/core/*.css deck.js/core/')
+    system('cp doconce_modifications/deck/themes/style/*.css deck.js/themes/style/')
+    # this find will always generate errors..., use os.system
+    os.system("find deck.js/extensions -name '.git' -exec rm -rf {} \;")
+    zip_dir('deck.js')
+
+    # Pack latex styles and figures in a zip file without any directory
+    # minted.sty and anslistings.sty are not copied from some
+    # repo every time, so get the latest versions from ptex2tex (manually)
+    os.chdir('latex_styles')
+    system('zip latex_styles.zip *.sty *.pdf *.pdf *.eps')
+    system('mv -f latex_styles.zip ..')
+    os.chdir(os.pardir)
+
+    for zfile in glob.glob('*.zip'):
+        shutil.copy(zfile, os.path.join(os.pardir, 'lib', 'doconce', zfile))
+
+    # back to root dir
+    os.chdir(os.pardir)
+
+    system('python bin/doconce insertdocstr plain lib/doconce')
+
+    # remove files that are to be regenerated:
+    #system('sh clean.sh')
+    thisdir = os.getcwd()
+    dirs = [os.path.join('lib', 'doconce', 'docstrings'),
+            os.path.join('doc', 'tutorial'),
+            os.path.join('doc', 'manual'),
+            os.path.join('doc', 'quickref'),
+            os.path.join('doc', 'slides'),
+            os.path.join('doc', 'blog'),
+            'test',
+            'bundled',
+            ]
+    for d in dirs:
+        os.chdir(d)
+        system('sh ./clean.sh')
+        os.chdir(thisdir)
+    print 'Successful execution of', sys.argv[0]
+
---
+++ lib/doconce/pandoc.py
@@ -0,0 +1,292 @@
+"""
+See http://johnmacfarlane.net/pandoc/README.html
+for syntax.
+"""
+
+import re, sys
+from common import default_movie, plain_exercise, table_analysis, \
+     insert_code_and_tex
+from html import html_movie
+
+def pandoc_author(authors_and_institutions, auth2index,
+                 inst2index, index2inst, auth2email):
+    # List authors on multiple lines
+    authors = []
+    for author, i, e in authors_and_institutions:
+        if i is None:
+            authors.append(author)
+        else:
+            authors.append(author + ' at ' + ' and '.join(i))
+    authors = '% ' + ';  '.join(authors) + '\n'
+    return authors
+
+
+def pandoc_code(filestr, code_blocks, code_block_types,
+                tex_blocks, format):
+    for i in range(len(tex_blocks)):
+        # Remove \[ and \] in single equations
+        tex_blocks[i] = tex_blocks[i].replace(r'\[', '')
+        tex_blocks[i] = tex_blocks[i].replace(r'\]', '')
+        # Check for illegal environments
+        m = re.search(r'\\begin\{(.+?)\}', tex_blocks[i])
+        if m:
+            envir = m.group(1)
+            if envir not in ('equation', 'align*', 'align'):
+                print """\
+*** warning: latex envir \\begin{%s} does not work well:
+    pandoc-extended markdown syntax handles only single equations
+    (but doconce splits align environments into single equations).
+    Labels in equations do not work with pandoc-extended markdown
+    output.
+""" % envir
+        # Add $$ on each side of the equation
+        tex_blocks[i] = '$$\n' + tex_blocks[i] + '\n$$'
+
+    filestr = insert_code_and_tex(filestr, code_blocks, tex_blocks, format)
+
+    defs = dict(cod='Python', pycod='Python', cppcod='Cpp',
+                fcod='Fortran', ccod='C',
+                pro='Python', pypro='Python', cpppro='Cpp',
+                fpro='Fortran', cpro='C',
+                sys='Bash', dat='Python',
+                pyoptpro='Python')
+        # (the "python" typesetting is neutral if the text
+        # does not parse as python)
+
+    # Code blocks apply the ~~~~~ delimiter, with blank lines before
+    # and after (alternative: indent code 4 spaces - not preferred)
+    for key in defs:
+        language = defs[key]
+        replacement = '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.%s}\n' % defs[key]
+        #replacement = '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.%s ,numberLines}\n' % defs[key]  # enable line numbering
+        #filestr = re.sub(r'^!bc\s+%s\s*\n' % key,
+        #                 replacement, filestr, flags=re.MULTILINE)
+        cpattern = re.compile(r'^!bc\s+%s\s*\n' % key, flags=re.MULTILINE)
+        filestr = cpattern.sub(replacement, filestr)
+
+    # any !bc with/without argument becomes an unspecified block
+    replacement = '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    cpattern = re.compile(r'^!bc.*$', flags=re.MULTILINE)
+    filestr = cpattern.sub(replacement, filestr)
+
+    cpattern = re.compile(r'^!ec\s*$', flags=re.MULTILINE)
+    filestr = cpattern.sub('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n', filestr)
+
+    filestr = re.sub(r'^!bt *$', '', filestr, flags=re.MULTILINE)
+    filestr = re.sub(r'^!et *$', '', filestr, flags=re.MULTILINE)
+
+    # \eqref and labels will not work, but labels do no harm
+    filestr = filestr.replace(' label{', ' \\label{')
+    pattern = r'^label\{'
+    filestr = re.sub(pattern, '\\label{', filestr, flags=re.MULTILINE)
+    filestr = re.sub(r'\(ref\{(.+?)\}\)', r'\eqref{\g<1>}', filestr)
+
+    # Final fixes
+
+    # Seems that title and author must appear on the very first lines
+    filestr = filestr.lstrip()
+
+    return filestr
+
+def pandoc_table(table):
+    """
+
+    Simple markdown tables look like this::
+
+        Left         Right   Center     Default
+        -------     ------ ----------   -------
+        12              12     12            12
+        123            123     123          123
+        1                1      1             1
+
+    """
+    # Slight modification of rst_table
+    # Here is the pandoc table format
+
+    column_width = table_analysis(table['rows'])
+    ncolumns = len(column_width)
+    column_spec = table.get('columns_align', 'c'*ncolumns).replace('|', '')
+    heading_spec = table.get('headings_align', 'c'*ncolumns).replace('|', '')
+    a2py = {'r': 'rjust', 'l': 'ljust', 'c': 'center'}
+    s = ''  # '\n'
+    for i, row in enumerate(table['rows']):
+        #s += '    '  # indentation of tables
+        if row == ['horizontal rule'] and i > 0 and i < len(table['rows'])-1:
+            # No horizontal rule at the top and bottom, just after heading
+            for w in column_width:
+                s += '-'*w + '  '
+        else:
+            # check if this is a headline between two horizontal rules:
+            if i == 1 and \
+               table['rows'][i-1] == ['horizontal rule'] and \
+               table['rows'][i+1] == ['horizontal rule']:
+                headline = True
+            else:
+                headline = False
+
+            for w, c, ha, ca in \
+                    zip(column_width, row, heading_spec, column_spec):
+                if headline:
+                    s += getattr(c, a2py[ha])(w) + '  '
+                elif row != ['horizontal rule']:
+                    s += getattr(c, a2py[ca])(w) + '  '
+        s += '\n'
+    s += '\n'
+    return s
+
+def pandoc_figure(m):
+    filename = m.group('filename')
+    caption = m.group('caption').strip()
+    text = '![%s](%s)' % (caption, filename)
+    #print 'pandoc_figure:', text
+    return text
+
+def pandoc_ref_and_label(section_label2title, format, filestr):
+    # .... see section ref{my:sec} is replaced by
+    # see the section "...section heading..."
+    pattern = r'[Ss]ection(s?)\s+ref\{'
+    replacement = r'the section\g<1> ref{'
+    filestr = re.sub(pattern, replacement, filestr)
+    pattern = r'[Cc]hapter(s?)\s+ref\{'
+    replacement = r'the chapter\g<1> ref{'
+    filestr = re.sub(pattern, replacement, filestr)
+    # Remove Exercise, Project, Problem in references since those words
+    # are used in the title of the section too
+    pattern = r'(the\s*)?([Ee]xercises?|[Pp]rojects?|[Pp]roblems?)\s+ref\{'
+    replacement = r' ref{'
+    filestr = re.sub(pattern, replacement, filestr)
+
+    # Remove label{...} from output (when only label{} on a line, remove
+    # the newline too, leave label in figure captions, and remove all the rest)
+    #filestr = re.sub(r'^label\{.+?\}\s*$', '', filestr, flags=re.MULTILINE)
+    cpattern = re.compile(r'^label\{.+?\}\s*$', flags=re.MULTILINE)
+    filestr = cpattern.sub('', filestr)
+    #filestr = re.sub(r'^(FIGURE:.+)label\{(.+?)\}', '\g<1>{\g<2>}', filestr, flags=re.MULTILINE)
+    cpattern = re.compile(r'^(FIGURE:.+)label\{(.+?)\}', flags=re.MULTILINE)
+    filestr = cpattern.sub('\g<1>{\g<2>}', filestr)
+    filestr = re.sub(r'label\{.+?\}', '', filestr)  # all the remaining
+
+    # Replace all references to sections. Pandoc needs a coding of
+    # the section header as link.
+    def title2pandoc(title):
+        # http://johnmacfarlane.net/pandoc/README.html
+        for c in ('?', ';', ':'):
+            title = title.replace(c, '')
+        title = title.replace(' ', '-').strip()
+        start = 0
+        for i in range(len(title)):
+            if title[i].isalpha():
+                start = i
+        title = title[start:]
+        title = title.lower()
+        if not title:
+            title = 'section'
+        return title
+
+    for label in section_label2title:
+        filestr = filestr.replace('ref{%s}' % label,
+                  '[%s](#%s)' % (section_label2title[label],
+                                 title2pandoc(section_label2title[label])))
+
+    return filestr
+
+
+def pandoc_index_bib(filestr, index, citations, bibfile):
+    # pandoc citations are of the form
+    # bla-bla, see [@Smith04, ch. 1; @Langtangen_2008]
+    # Method: cite{..} -> [...], doconce.py has already fixed @ and ;
+    filestr = re.sub(r'cite\{(.+?)\}', r'[\g<1>]', filestr)
+
+    #filestr = re.sub(r'^BIBFILE:.+$', bibtext, filestr, flags=re.MULTILINE)
+    cpattern = re.compile(r'^BIBFILE:.+$', flags=re.MULTILINE)
+    filestr = cpattern.sub('# References', filestr)
+
+    # pandoc does not support index entries,
+    # remove all index entries
+
+    filestr = re.sub(r'idx\{.+?\}' + '\n?', '', filestr)
+    return filestr
+
+def pandoc_quote(block, format):
+    # block quotes in pandoc start with "> "
+    lines = []
+    for line in block.splitlines():
+        lines.append('> ' + line)
+    return '\n'.join(lines) + '\n\n'
+
+def define(FILENAME_EXTENSION,
+           BLANKLINE,
+           INLINE_TAGS_SUBST,
+           CODE,
+           LIST,
+           ARGLIST,
+           TABLE,
+           EXERCISE,
+           FIGURE_EXT,
+           CROSS_REFS,
+           INDEX_BIB,
+           TOC,
+           ENVIRS,
+           INTRO,
+           OUTRO):
+    # all arguments are dicts and accept in-place modifications (extensions)
+
+    FILENAME_EXTENSION['pandoc'] = '.md'
+    BLANKLINE['pandoc'] = '\n'
+    # replacement patterns for substitutions of inline tags
+    INLINE_TAGS_SUBST['pandoc'] = {
+        'math':      None,  # indicates no substitution, leave as is
+        'math2':     r'\g<begin>$\g<latexmath>$\g<end>',
+        'emphasize': None,
+        'bold':      None,
+        'figure':    pandoc_figure,
+        #'movie':     default_movie,
+        'movie':     html_movie,
+        'verbatim':  None,
+        #'linkURL':   r'\g<begin>\g<link> (\g<url>)\g<end>',
+        'linkURL2':  r'[\g<link>](\g<url>)',
+        'linkURL3':  r'[\g<link>](\g<url>)',
+        'linkURL2v': r'[`\g<link>`](\g<url>)',
+        'linkURL3v': r'[`\g<link>`](\g<url>)',
+        'plainURL':  r'<\g<url>>',
+        # "Reference links" in pandoc are not yet supported
+        'title':     r'% \g<subst>',
+        'author':    pandoc_author,
+        'date':      '% \g<subst>\n',
+        'chapter':       lambda m: r'\g<subst>\n%s' % ('%'*len(m.group('subst').decode('utf-8'))),
+        'section':       lambda m: r'\g<subst>\n%s' % ('='*len(m.group('subst').decode('utf-8'))),
+        'subsection':    lambda m: r'\g<subst>\n%s' % ('-'*len(m.group('subst').decode('utf-8'))),
+        'subsubsection': lambda m: r'\g<subst>\n%s' % ('~'*len(m.group('subst').decode('utf-8'))),
+        'paragraph':     r'*\g<subst>* ',  # extra blank
+        'abstract':      r'*\g<type>.* \g<text>\n\n\g<rest>',
+        'comment':       '<!-- %s -->',
+        }
+
+    CODE['pandoc'] = pandoc_code
+    ENVIRS['pandoc'] = {
+        'quote':        pandoc_quote,
+        }
+
+    from common import DEFAULT_ARGLIST
+    ARGLIST['pandoc'] = DEFAULT_ARGLIST
+    LIST['pandoc'] = {
+        'itemize':
+        {'begin': '', 'item': '*', 'end': '\n'},
+
+        'enumerate':
+        {'begin': '', 'item': '%d.', 'end': '\n'},
+
+        'description':
+        {'begin': '', 'item': '%s\n  :   ', 'end': '\n'},
+
+        'separator': '\n',
+        }
+    CROSS_REFS['pandoc'] = pandoc_ref_and_label
+
+    TABLE['pandoc'] = pandoc_table
+    INDEX_BIB['pandoc'] = pandoc_index_bib
+    EXERCISE['pandoc'] = plain_exercise
+    TOC['pandoc'] = lambda s: '# Table of contents: Run pandoc with --toc option'
+    FIGURE_EXT['pandoc'] = ('.png', '.gif', '.jpg', '.jpeg', '.tif', '.tiff', '.pdf')
+
+    # no return, rely on in-place modification of dictionaries
