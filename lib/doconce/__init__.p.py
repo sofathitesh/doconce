d33b940d2fd42a120610cc9272cf3d900ce38fb0
Hans Petter Langtangen <hpl@simula.no>
2013-04-09 08:49:06 +0200

    Tagul logo.

---
+++ lib/doconce/__init__.p.py
@@ -0,0 +1,15 @@
+'''
+
+# #include "docstrings/docstring.dst.txt"
+
+'''
+
+__version__ = '0.9.0'
+version = __version__
+__author__ = 'Hans Petter Langtangen', 'Johannes H. Ring'
+author = __author__
+
+__acknowledgments__ = ''
+
+from doconce import main
+
